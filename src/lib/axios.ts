44f876e935041c9fb74f5f398152d7616ae2d94a
Sergio Winkelstroter <winksousa0@gmail.com>
2024-05-21 17:55:52 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cashcare.cloud/api",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api"
+      : "https://cashcare.cloud/api",
   // baseURL: "http://localhost:3000/api",
 });
