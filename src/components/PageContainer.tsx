46356a2d8202681ef56bbc4266166b44cfaea2da
Sergio Winkelstroter <winksousa0@gmail.com>
2024-05-20 21:57:46 -0300

    Update PageContainer.tsx

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -3,7 +3,7 @@ import { NavigationViewer } from "./NavigationViewer";
 
 export const PageContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex min-h-screen my-4 sm:mt-0 flex-col md:ml-16">
+    <div className="flex min-h-screen my-4 sm:mt-0 flex-col sm:ml-16">
       {children}
     </div>
   );
